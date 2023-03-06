bc044dc37f3d509aa3631ab1b12cb9dfc8064dc3
VineetTambe <tambevineet@gmail.com>
2023-03-05 20:52:21 -0500

    added position controller

--- controls/panda_controller.py
+++ controls/panda_controller.py
@@ -1,16 +1,26 @@
 import numpy as np
+import mujoco as mj
 
 class FrankaController:
 	def __init__(self):
+		self.desired_cartesian_positions = np.array([0,0,0])
+		self.desired_cartesian_velocities = np.array([0,0,0])
 		self.desired_joint_positions = np.array([0,0,0,0,0,0,0])
 		self.desired_joint_velocities = np.array([0,0,0,0,0,0,0])
 
-	def setGoalStates(self, desired_joint_positions = np.array([0,0,0,0,0,0,0]), desired_joint_velocities = np.array([0,0,0,0,0,0,0])):
-		self.desired_joint_positions = desired_joint_positions
-		self.desired_joint_velocities = desired_joint_velocities
+	def setGoalStates(self, 
+			desired_cartesian_pos = np.array([0,0,0]),
+			desired_cartesian_vel = np.array([0,0,0]),
+			desired_joint_pos = np.array([0,0,0,0,0,0,0]), 
+			desired_joint_vel = np.array([0,0,0,0,0,0,0])):
+		
+		self.desired_cartesian_positions = desired_cartesian_pos
+		self.desired_cartesian_velocities = desired_cartesian_vel
+		self.desired_joint_positions = desired_joint_pos
+		self.desired_joint_velocities = desired_joint_vel
 		return
 
-	def positionControlSim(self, model, data):
+	def jointPositionControlSim(self, model, data):
 
 		# Instantite a handle to the desired body on the robot
 		body = data.body("hand")
@@ -24,11 +34,55 @@ class FrankaController:
 		# Set the actuator control torques
 		data.ctrl[:7] = data.qfrc_bias[:7] + Kp*(self.desired_joint_positions-data.qpos[:7]) + Kd*((self.desired_joint_velocities)-data.qvel[:7])
 
+	# Control callback for an impedance controller
+	def cartesianPositionControlSim(self, model, data): #TODO:
+		try :
+			# Get the Jacobian at the desired location on the robot
+			jacp = np.zeros((3, model.nv))
+			jacr = np.zeros((3, model.nv))
+			bodyid = mj.mj_name2id(model, mj.mjtObj.mjOBJ_BODY, 'hand')
+			mj.mj_jacBody(model, data, jacp, jacr, bodyid)
+			full_jacobian = np.concatenate((jacp,jacr))
+
+			# Instantite a handle to the desired body on the robot
+			Kd = 0.750
+			Kp = 8.0
+
+			curr_pos = data.xpos[bodyid]
+			# curr_vel = (curr_pos - prev_pos) / (curr_time - prev_time)
+			handVel = np.zeros((6,))
+
+			curr_vel = np.zeros((3,))
+			mj.mj_objectVelocity(model,data, mj.mjtObj.mjOBJ_BODY, bodyid, handVel[:6], True)
+			curr_vel = handVel[:3]
+			# Get the position error
+			error_pos = self.desired_cartesian_positions - curr_pos
+			error_vel = self.desired_cartesian_velocities - curr_vel
+
+			# This function works by taking in return parameters!!! Make sure you supply it with placeholder
+			# variables
+			des_force = np.zeros((6,1))
+			des_force[:3] = (Kp * error_pos + Kd * error_vel).reshape(3,-1) 
+			des_force[1] = 0
+			des_force[2] = 0
+
+			# Compute the impedance control input torques
+			calculated_torques = np.squeeze((full_jacobian.T @ des_force))
+
+			# Set the control inputs
+			data.ctrl[:7] = data.qfrc_bias[:7] + calculated_torques
+		except Exception as e:
+			print(e)
+
+	def directControlSim(self, desired_pose):
+		# TODO directly set the desired_pose to the sim pose -> this is just for simulation purpose.
+		pass
+
 	def jointPosControl(self,desired_joint_positions):
 		# TODO directly set the desired joint positions using the franka py control library here.
 		pass
 
 	
-	def cartecianPosControl(self,desired_positions):
-		# TODO directly set the desired cartecian positions using the franka py control library here.
+	def cartesianPosControl(self,desired_positions):
+		# TODO directly set the desired cartesian positions using the franka py control library here.
 		pass
--- test/test_sim.py
+++ test/test_sim.py
@@ -1,27 +1,38 @@
 import sys
-sys.path.insert(0, '..')
+import os 
 
+fpath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, fpath + '/..')
 from controls import panda_controller
 import mujoco as mj
 from mujoco import viewer
-import os 
+
 import numpy as np
 
 if __name__ == "__main__":
 
-	fpath = os.path.dirname(os.path.abspath(__file__))
+	
 	xml_filepath = fpath + '/../assets/franka_emika_panda/panda_nohand_torque.xml'
-	controlObj = panda_controller.FrankaController()
 
-	des_joint_positions = np.array([0.5740863127850836, 0.31159654210717075, -0.679618955563617, -2.078791138726525, -0.11481848580697593, 3.914113692658701, -2.864637256298371])
-	controlObj.setGoalStates(desired_joint_positions = des_joint_positions)
 	# Load the xml file here
 	model = mj.MjModel.from_xml_path(xml_filepath)
 	data = mj.MjData(model)
 
 	# Set the simulation scene to the home configuration
 	mj.mj_resetDataKeyframe(model, data, 0)
-	mj.set_mjcb_control(controlObj.positionControlSim)
 
+	controlObj = panda_controller.FrankaController()
+	# test for joint position control ----------------------------------------------
+	
+	# des_joint_positions = np.array([0.5740863127850836, 0.31159654210717075, -0.679618955563617, -2.078791138726525, -0.11481848580697593, 3.914113692658701, -2.864637256298371])
+	# controlObj.setGoalStates(desired_joint_positions = des_joint_positions)
+	# mj.set_mjcb_control(controlObj.jointPositionControlSim)
+
+	#----------------------------------------------------------------------
+
+	# test for cartesian position control ----------------------------------------------
+	des_pos = np.array([0.7 ,0.7 ,00.0])
+	controlObj.setGoalStates(desired_cartesian_pos = des_pos)
+	mj.set_mjcb_control(controlObj.cartesianPositionControlSim)
 	# Launch the simulate viewer
 	viewer.launch(model, data)
