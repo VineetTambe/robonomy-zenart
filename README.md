62e60d0d266f9496c18e43f6c7c4958a50518e65
Praveen Venkatesh <30774147+praveenVnktsh@users.noreply.github.com>
2023-03-28 11:28:29 -0400

    added init stuff

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # robonomy
 Name is subject to change.
+
+
+## Data
+
+put the data under data folder. ndjsons pls
---
+++ parse_img.py
@@ -0,0 +1,47 @@
+import struct
+from struct import unpack
+import numpy as np
+# import ndjson
+def unpack_drawing(file_handle):
+    key_id, = unpack('Q', file_handle.read(8))
+    country_code, = unpack('2s', file_handle.read(2))
+    recognized, = unpack('b', file_handle.read(1))
+    timestamp, = unpack('I', file_handle.read(4))
+    n_strokes, = unpack('H', file_handle.read(2))
+    image = []
+    for i in range(n_strokes):
+        n_points, = unpack('H', file_handle.read(2))
+        fmt = str(n_points) + 'B'
+        x = unpack(fmt, file_handle.read(n_points))
+        y = unpack(fmt, file_handle.read(n_points))
+        image.append((x, y))
+
+    return {
+        'key_id': key_id,
+        'country_code': country_code,
+        'recognized': recognized,
+        'timestamp': timestamp,
+        'image': image
+    }
+
+
+def unpack_drawings(filename):
+    with open(filename, 'rb') as f:
+        while True:
+            try:
+                yield unpack_drawing(f)
+            except struct.error:
+                break
+
+
+for drawing in unpack_drawings('data/full_raw_The Eiffel Tower.ndjson'):
+    # do something with the drawing
+    # print(drawing['image'])
+    print(drawing.keys())
+    # print(drawing['timestamp'])
+    # img = (drawing['image'])
+    # for i in img:
+    #     print(i)
+    exit()
+
+
